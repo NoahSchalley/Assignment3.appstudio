73cc78eb839f0b17ec5f569993dd464ce0174512
NoahSchalley <70349070+NoahSchalley@users.noreply.github.com>
2021-03-14 21:29:05 -0500

    Form-2

---
+++ forms/forReverseStates/forReverseStates.js
@@ -0,0 +1,8 @@
+let stateList = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"]
+stateList.reverse()
+
+
+
+for(let i = 0; i < stateList.length; i++) {
+  console.log(stateList[i])
+}
